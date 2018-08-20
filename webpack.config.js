faa44718f66309b8d02e87a9a24a18cdfd42bb71
Alexander Bjerkan <alexander.bjerkan@gmail.com>
2018-08-20 20:58:57 +0200

    Setup webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,28 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
-const outputDir = path.join(__dirname, "build/");
+
+const outputDir = path.join(__dirname, 'build/');
 
 const isProd = process.env.NODE_ENV === 'production';
 
 module.exports = {
   entry: './src/Index.bs.js',
   mode: isProd ? 'production' : 'development',
+  devServer: {
+    //hot: true,
+    contentBase: path.join(__dirname, 'public'),
+    port: 3000,
+    historyApiFallback: true
+    //disableHostCheck: true,
+  },
   output: {
     path: outputDir,
-    publicPath: outputDir,
-    filename: 'Index.js',
+    publicPath: '/',
+    filename: isProd ? '[name].[chunkhash].js' : '[name].js'
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, 'src', 'index.html')
+    })
+  ]
 };
